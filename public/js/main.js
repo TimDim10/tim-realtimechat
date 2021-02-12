a6ec4f10d1e0ee8d7d0e22942b550b6cb837da18
timdim10 <76626121+TimDim10@users.noreply.github.com>
2021-02-12 20:43:59 +0200

    Add files via upload

---
+++ public/js/main.js
@@ -0,0 +1,75 @@
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
+const userNameHeader = document.getElementById('usernameTitle');
+
+
+
+//Get username and room from URL
+const urlParams = new URLSearchParams(window.location.search);
+const username = urlParams.get('username');
+const room = urlParams.get('room');
+
+const socket = io();
+
+//Join chatroom
+socket.emit('joinRoom' , { username , room });
+
+//Get room and users
+socket.on('roomUsers' , ({room , users , username}) => {
+    outputRoomName(room);
+    outputUsers(users);
+    outPutUserOnHeader(username);
+});
+//Message from server
+socket.on('message' , message => {
+    outputMessage(message);
+
+    //Scroll down
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+
+})
+
+//Message submit
+chatForm.addEventListener('submit' , e => {
+    e.preventDefault();
+
+    //get message text
+    const msg = e.target.elements.msg.value;
+
+    //Emit message to server
+    socket.emit('chatMessage' , msg);
+
+    //Clear input
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus();
+});
+
+//Output message to DOM
+function outputMessage(message){
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
+    <p class="text">
+      ${message.text}
+    </p>`;
+    document.querySelector('.chat-messages').appendChild(div);
+}
+
+//Add room name to DOM
+function outputRoomName(room){
+    roomName.innerText = room;
+}
+
+//Add users to DOM
+function outputUsers(users){
+    userList.innerHTML = `
+        ${users.map(user => `<li>${user.username}</li>`).join('')}
+    `;
+}
+
+//Add username to DOM
+function outPutUserOnHeader(username){
+    userNameHeader.innerText = "";
+}
